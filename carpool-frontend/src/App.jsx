7e2e483f1dde6a35de58c1f28016cf00aa90ed40
Vapaa Eero <eero.hallikainen@gmail.com>
2025-03-14 13:50:26 +0200

    fix

--- carpool-frontend/src/App.jsx
+++ carpool-frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useContext } from 'react'
 import {
   BrowserRouter as Router,
@@ -46,42 +45,3 @@ const App = () => {
 }
 
 export default App
-
-
-=======
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
->>>>>>> 8325f8ade864eee6e7af0bc46d995d5e52c6960b
